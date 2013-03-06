353292de70f0b7d89878da394e3a893f0d0278f4
Masaki Muranaka <monaka@monami-ya.jp>
2013-03-06 17:37:53 +0900

    Add String#getbyte support. This is based on @akiray03's patch (70fe49af306730059fdc6e2f97df645a8e256bcb) Reported on mruby/mruby#956.

--- src/string.c
+++ src/string.c
@@ -7,6 +7,27 @@
 #include "mruby.h"
 #include "mruby/string.h"
 
+/* mrb_str_dump is in mruby core. */
+
+static mrb_value
+mrb_str_getbyte(mrb_state *mrb, mrb_value str)
+{
+  mrb_int idx;
+  struct RString *s;
+  mrb_value str2;
+  unsigned char *p;
+
+  mrb_get_args(mrb, "i", &idx);
+  str2 = mrb_str_substr(mrb, str, idx, 1);
+  if (mrb_nil_p(str2) || RSTRING_LEN(str2) == 0) {
+    return mrb_nil_value();
+  }
+
+  s = mrb_str_ptr(str2);
+  p = (unsigned char *)(s->ptr);
+
+  return mrb_fixnum_value(p[0]);
+}
 
 
 /*********************************************************************/
@@ -20,6 +41,7 @@ mrb_mruby_string_enhancement_gem_init(mrb_state* mrb)
   s = mrb->string_class;
 
   mrb_define_method(mrb, s, "dump",  mrb_str_dump, ARGS_NONE());
+  mrb_define_method(mrb, s, "getbyte",         mrb_str_getbyte,         ARGS_REQ(1));
 }
 
 void
